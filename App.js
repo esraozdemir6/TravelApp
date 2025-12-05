46d186a9a0c543279d2335793066b34e93ae9b87
esraozdemir6 <esraozddemir@gmail.com>
2025-12-05 18:48:45 +0300

    feat: add icons and custom styling to navigation (Part 5)

--- App.js
+++ App.js
@@ -5,26 +5,52 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import HomeScreen from './screens/HomeScreen';
 import DetailsScreen from './screens/DetailsScreen';
 import SettingsScreen from './screens/SettingsScreen'; 
+import { Ionicons } from '@expo/vector-icons';
+
 
 const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
 
 function HomeStack() {
   return (
-    <Stack.Navigator>
+    <Stack.Navigator
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: 'tomato', 
+        },
+        headerTintColor: 'white', 
+      }}
+    >
       <Stack.Screen name="Home" component={HomeScreen} />
       <Stack.Screen name="Details" component={DetailsScreen} />
     </Stack.Navigator>
   );
 }
 
+
 export default function App() {
   return (
     <NavigationContainer>
-      <Tab.Navigator screenOptions={{ headerShown: false }}>
-        <Tab.Screen name="Explore" component={HomeStack} />
-        <Tab.Screen name="Settings" component={SettingsScreen} />
-      </Tab.Navigator>
+      <Tab.Navigator
+  screenOptions={({ route }) => ({
+    headerShown: false, 
+    tabBarIcon: ({ focused, color, size }) => {
+      let iconName;
+      if (route.name === 'Explore') {
+        iconName = focused ? 'map' : 'map-outline'; 
+      } else if (route.name === 'Settings') {
+        iconName = focused ? 'settings' : 'settings-outline'; 
+      }
+      return <Ionicons name={iconName} size={size} color={color} />;
+    },
+    tabBarActiveTintColor: 'tomato', 
+    tabBarInactiveTintColor: 'gray', 
+  })}
+>
+  <Tab.Screen name="Explore" component={HomeStack} />
+  <Tab.Screen name="Settings" component={SettingsScreen} />
+</Tab.Navigator>
+
     </NavigationContainer>
   );
 }
